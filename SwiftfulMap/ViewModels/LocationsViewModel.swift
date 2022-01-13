c916009bc6ed9512c9e32c1f4b2de7c71c6a2bb7
MIKKILINENI PAVAN KUMAR <pavanmikkilineni690@gmail.com>
2022-01-13 20:50:33 +0530

    Animated Locations List View Added

--- SwiftfulMap/ViewModels/LocationsViewModel.swift
+++ SwiftfulMap/ViewModels/LocationsViewModel.swift
@@ -6,13 +6,48 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel : ObservableObject{
    
     @Published var locations:[Location]
+    @Published var mapLocation:Location{
+        didSet{
+            updateMKCoordinateRegion(location: mapLocation)
+        }
+    }
+    @Published var mkCoordinateRegion:MKCoordinateRegion = MKCoordinateRegion()
+    
+    @Published var showLocationsList:Bool = false
+    
+    let mapSpan:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init(){
-        self.locations = LocationsDataService.locations
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMKCoordinateRegion(location: mapLocation)
+    }
+    
+    func updateMKCoordinateRegion(location:Location){
+        withAnimation(.easeInOut){
+            mkCoordinateRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan
+                )
+        }
+    }
+    
+    func toggleShowLocationsList(){
+        withAnimation(.easeInOut){
+            showLocationsList.toggle()
+        }
+    }
+    
+    func showNextLocation(location:Location){
+        mapLocation = location
+        showLocationsList.toggle()
     }
     
     
--- SwiftfulMap/Views/LocationsView.swift
+++ SwiftfulMap/Views/LocationsView.swift
@@ -6,17 +6,54 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationsView: View {
     
     @EnvironmentObject private var viewModel:LocationsViewModel
     
     var body: some View {
-        List{
-            ForEach(viewModel.locations){
-                Text($0.name)
+        ZStack{
+            Map(coordinateRegion:$viewModel.mkCoordinateRegion)
+                .ignoresSafeArea()
+            VStack(spacing:0){
+                header
+                    .padding()
+                Spacer()
             }
         }
+    }
+}
+
+extension LocationsView{
+    private var header: some View{
+        VStack{
+            Button(action:viewModel.toggleShowLocationsList){
+                Text("\(viewModel.mapLocation.name), \(viewModel.mapLocation.cityName)")
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .frame(height:55)
+                    .frame(maxWidth:.infinity)
+                    .overlay(alignment:.leading){
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                            .padding()
+                            .rotationEffect(
+                                Angle(
+                                    degrees: viewModel.showLocationsList ? 180 : 0
+                                )
+                            )
+                    }
+            }
+            if viewModel.showLocationsList{
+                LocationsListView()
+            }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
     }
 }
 
