877026ff2473f258ebb9ab9da359ae224d34d67a
MIKKILINENI PAVAN KUMAR <pavanmikkilineni690@gmail.com>
2022-01-13 04:15:31 -0800

    Initial Commit

---
+++ SwiftfulMap/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftfulMap
+//
+//  Created by Pavan Mikkilineni on 13/01/22.
+//
+
+import Foundation
+import MapKit
+
+struct Location{
+    let name:String
+    let cityName:String
+    let coordinates:CLLocationCoordinate2D
+    let description:String
+    let imageNames:[String]
+    let link:String
+}
---
+++ SwiftfulMap/SwiftfulMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapApp.swift
+//  SwiftfulMap
+//
+//  Created by Pavan Mikkilineni on 13/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LocationView()
+        }
+    }
+}
